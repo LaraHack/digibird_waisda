6a70c09dd9a21660cff30ab10fce9b328480906d
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2016-09-05 16:07:29 +0200

    installed nodemon as a dependency

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
