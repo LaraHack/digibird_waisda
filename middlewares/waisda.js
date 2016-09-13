da35fa141bbd1336710d804c4a62711a4299c684
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2016-09-13 13:04:44 +0200

    conenction to db, call stored procedure with and without parameters, fixes #7, #10, #11

--- middlewares/waisda.js
+++ middlewares/waisda.js
@@ -5,33 +5,74 @@ module.exports = {
   mysql_connect: function () {
     var connection = mysql.createConnection({
       host: credentials.HOST,
-      user: credentials.USER,
+      user: credentials.USERNAME,
       password: credentials.PASSWORD,
       database: credentials.DATABASE
     });
 
+    console.log("username:", credentials.USERNAME);
     //connection.connect();
 
     connection.connect(function(err){
-      if (err) {
+      if (!err) {
+        console.log('connected as id ' + connection.threadId);
+      } else {
         console.error('error connecting: ' + err.stack);
         return;
       }
-
-      console.log('connected as id ' + connection.threadId);
     });
-    // connection.query("SELECT * from Video", function(err, rows, fields) {
+
+    // connection.query('SELECT * from ' + credentials.DATABASE + '.Video;',
+    //   function(err, rows, fields) {
     //   if (!err) {
     //     console.log("The solution is: ", rows);
+    //     console.log("The fields are: ", fields);
     //   } else {
     //     console.log("Error while performing Query.");
-    //   });
-    // connection.query('CALL insert_video(parms)',function(err,rows){
-    //   if(err) throw err;
-    //
-    //   console.log('Data received from Db:\n');
-    //   console.log(rows[0]);
+    //     return;
+    //   }
     // });
+
+    // var params = "'test5','000000', NULL, 0, 'JW', NULL, NULL, NULL, NULL";
+    // connection.query('CALL ' + credentials.DATABASE + '.sp_insert_video (' + params + ');',
+    //   function(err, rows, fields) {
+    //     if (!err) {
+    //       console.log('Data received from Db:\n');
+    //       console.log(rows);
+    //       // OkPacket {
+    //         // fieldCount: 0,
+    //         // affectedRows: 1,
+    //         // insertId: 0,
+    //         // serverStatus: 2,
+    //         // warningCount: 0,
+    //         // message: '',
+    //         // protocol41: true,
+    //         // changedRows: 0 }
+    //         console.log(fields); //undefined
+    //     } else {
+    //       console.log("Error while performing Query.");
+    //       return;
+    //     }
+    // });
+
+    connection.query('CALL ' + credentials.DATABASE + '.sp_select_video ();',
+      function(err, rows, fields) {
+        if (!err) {
+          console.log('Data received from Db:\n');
+          console.log("rows\n", rows[0]);
+          for (var i = 0; i < rows[0].length; i++) {
+            console.log("\nrecord ", i+1);
+            console.log("title:", rows[0][i].title);
+            console.log("image:", rows[0][i].imageUrl);
+            console.log("video:", rows[0][i].sourceUrl);
+            console.log("__________");
+          };
+          //console.log("\nfields\n", fields);
+        } else {
+          console.log("Error while performing Query.");
+          return;
+        }
+    });
 
     connection.end(function(err){
     // Do something after the connection is gracefully terminated.
--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,17 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  var currentTime = new Date();
-  var formattedTime = currentTime.getHours() + ":" +
-                      currentTime.getMinutes();
-  res.render('home', {
-    time: formattedTime
-  });
+  // run every minute
+  setInterval(function() {
+    var currentTime = new Date();
+    var formattedTime = currentTime.getHours() + ":" +
+                        currentTime.getMinutes();
+    res.render('home', {
+      time: formattedTime
+    });
+      // 1 minute delay
+  }, 600000);
+
 });
 
 module.exports = router;
