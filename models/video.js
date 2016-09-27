cae42be710145a71ba4360d7457440d462876547
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2016-09-27 16:13:32 +0200

    get total number of videos, resolves #17

--- models/video.js
+++ models/video.js
@@ -14,20 +14,54 @@ Endpoint | Request type | Method
 /*******************************************************************************/
 var connection = require('../middlewares/connection');
 
+// connection.query('CALL ' + credentials.DATABASE + '.sp_select_video ();',
+//   function(err, rows, fields) {
+//     if (!err) {
+//       console.log('Data received from Db:\n');
+//       console.log("rows\n", rows[0]);
+//       for (var i = 0; i < rows[0].length; i++) {
+//         console.log("\nrecord ", i+1);
+//         console.log("title:", rows[0][i].title);
+//         console.log("image:", rows[0][i].imageUrl);
+//         console.log("video:", rows[0][i].sourceUrl);
+//         console.log("__________");
+//       };
+
 module.exports = {
   getNoVideos: function (res) {
     connection.acquirePool(function(err, connection) {
         if (!err) {
           var threadId = connection.threadId;
-          console.log('connected as id ' + threadId);
-          //connection.query();
+          var db = connection.config.database;
+
+          var queryNoVideos = 'SELECT COUNT(*) AS no_videos from ' + db + '.Video;'
+          connection.query(queryNoVideos, function(err, rows, fields) {
+              if (!err) {
+                // get name of returned variable that contains the result
+                var nameVarNoVideos = fields[0].name;
+                console.log(typeof nameVarNoVideos);
+                console.log(nameVarNoVideos);
+                var noVideos = rows[0][nameVarNoVideos];
+
+                var jsonResponse = {
+                  'db': db,
+                  'threadId': threadId
+                };
+                jsonResponse[nameVarNoVideos] = noVideos;
+
+                console.log(jsonResponse);
+
+                // res.send("db: " + db + " | threadId: " + threadId + " | " +
+                // nameVarNoVideos + " = " + noVideos);
+                res.send(JSON.stringify(jsonResponse));
+              }
+            });
 
           // release it
           connection.release();
-          // connection released ot the pool
+          // connection released to the pool
           console.log("Connection released!");
-          //return threadId;
-          res.send("threadId:" + threadId);
+          // res.send("db:" + db + "| threadId:" + threadId + "|" + noVideos);
         } else {
           console.error('error connecting: ' + err.stack);
           //return err.stack;
