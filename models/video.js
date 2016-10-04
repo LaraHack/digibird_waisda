0d08686faa2eae06a4554f0b1487ba900b03d80d
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2016-10-04 16:42:33 +0200

    refactored a bit the code, resolves #18, resolves #19

--- models/video.js
+++ models/video.js
@@ -1,4 +1,4 @@
-/*/*****************************************************************************
+/*******************************************************************************
 video
 
 Database operations regarding the Video table
@@ -6,49 +6,120 @@ Database operations regarding the Video table
 Endpoint | Request type | Method
 
 /video | GET | getNoVideos
-/video?enabled=1 | GET | getNoEnabledVideos
+/video?enabled=true | GET | getNoEnabledVideos
+/video/title/:title | GET | getVideo(':title')
 /video | POST | insertVideo
-/video?id='xxx' | GET | getVideo('xxx')
 /dict | POST |  insertDictEntry
 /synonym | POST | insertSynonym
 /*******************************************************************************/
 var connection = require('../middlewares/connection');
 
+// query options
+var options = {
+  sql: '',
+  timeout: 30000, // 30s
+  values: []
+};
+
 module.exports = {
+  // GET total number of videos
   getNoVideos: function (res) {
     connection.acquirePool(function(err, connection) {
         if (!err) {
-          var threadId = connection.threadId;
           var db = connection.config.database;
-
           var queryNoVideos = 'SELECT ' + db + '.sf_no_videos();';
 
-          var options = {
-            sql: queryNoVideos,
-            timeout: 30000, // 30s
-          };
+          options.sql = queryNoVideos;
 
           connection.query(options, function(err, rows, fields) {
               if (!err) {
                 // get name of returned variable that contains the result
                 var nameVarNoVideos = fields[0].name;
+                // get result
                 var noVideos = rows[0][nameVarNoVideos];
-
+                // stringify result
                 var jsonResponse = JSON.stringify({'noVideos': noVideos});
 
+                // send json response
                 res.send(jsonResponse);
               }
             });
 
-          // release it
+          // release the connection
           connection.release();
           // connection released to the pool
           console.log("Connection released!");
         } else {
           console.error('error connecting: ' + err.stack);
-          //return err.stack;
           res.send(err.stack);
         }
     });
-    }
+  },
+
+  // GET number of videos that are enabled
+  getNoEnabledVideos: function (res) {
+    connection.acquirePool(function(err, connection) {
+        if (!err) {
+          var db = connection.config.database;
+          var queryNoEnabledVideos = 'SELECT ' + db + '.sf_no_enabled_videos();';
+
+          options.sql = queryNoEnabledVideos;
+
+          connection.query(options, function(err, rows, fields) {
+              if (!err) {
+                // get name of returned variable that contains the result
+                var nameVarNoVideos = fields[0].name;
+                // get result
+                var noVideos = rows[0][nameVarNoVideos];
+                // stringify result
+                var jsonResponse = JSON.stringify({'noVideos': noVideos});
+
+                // send json response
+                res.send(jsonResponse);
+              }
+            });
+
+          // release the connection
+          connection.release();
+          // connection released to the pool
+          console.log("Connection released!");
+        } else {
+          console.error('error connecting: ' + err.stack);
+          res.send(err.stack);
+        }
+      });
+    },
+
+    // GET videos with a title that contains a certain text
+    getTitleVideos: function (params, res) {
+      connection.acquirePool(function(err, connection) {
+          if (!err) {
+            var db = connection.config.database;
+            var queryTitleVideos = 'CALL ' + db + '.sp_select_videos_title(?);';
+
+            options.sql = queryTitleVideos;
+            options.values = params.title;
+
+            console.log();
+
+            connection.query(options, function(err, rows, fields) {
+                if (!err) {
+                  // get result and stringify it in JSON format
+                  var jsonResponse = JSON.stringify(rows[0]);
+
+                  // send json response
+                  res.send(jsonResponse);
+                }
+              });
+
+            // release the connection
+            connection.release();
+            // connection released to the pool
+            console.log("Connection released!");
+          } else {
+            console.error('error connecting: ' + err.stack);
+            res.send(err.stack);
+          }
+        });
+      }
 };
--- routes/video.js
+++ routes/video.js
@@ -21,4 +21,16 @@ router.get('/', function(req, res) {
   video.getNoVideos(res);
 });
 
+// GET total number of enabled videos
+router.get('/enabled', function(req, res) {
+  // send a resource
+  video.getNoEnabledVideos(res);
+});
+
+// GET videos with similar title
+router.get('/title/:title', function(req, res) {
+  // send a resource
+  video.getTitleVideos(req.params, res);
+});
+
 module.exports = router;
